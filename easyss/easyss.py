afa852fad74fad6f6b072a5ae58d81ea956468f5
EC2 Default User <ec2-user@ip-172-31-19-55.us-west-2.compute.internal>
2020-06-23 02:02:04 +0000

    adding skipping pending snapshots

--- easyss/easyss.py
+++ easyss/easyss.py
@@ -16,6 +16,11 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
+
 @click.group()
 def cli():
     """easyss manages snapshots"""
@@ -85,6 +90,10 @@ def create_snapshots(project):
         i.stop()
         i.wait_until_stopped()
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("   Skipping {0}, snapshot already in progress".format(v.id))
+                continue
+                
             print("creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by Easy Snapshot tool")
         print("starting {0}...".format(i.id))
